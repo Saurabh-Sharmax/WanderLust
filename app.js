1250ad18148b44ef3eea115c432f86154a1de8df
Saurabh Sharma <saurabhsharmax26@gmail.com>
2025-10-23 14:39:02 +0530

    Replace the * with /.*/

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.all("*", (req, res, next) => {
+app.all(/.*/, (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
